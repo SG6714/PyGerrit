ab41e9bf96563e1d39e1d7b1fafc523adea55208
Guignards <shane.guignard@zebra.com>
2024-11-21 12:27:48 -0500

    Update changes.py

--- api/changes.py
+++ api/changes.py
@@ -7,11 +7,11 @@ class GerritChangeManager:
         self.gerrit_url = gerrit_url
         self.auth = HTTPBasicAuth(username, password)
 
-    def get_changes(self):
+    def get_changes(self, limit):
         count = 0
         change_list = []
         while True:
-            query = f'/a/changes/?q=is:open&start={count}&limit=500&o=ALL_COMMITS'
+            query = f'/a/changes/?q=is:open&start={count}&limit={limit}&o=ALL_COMMITS'
             response = requests.get(self.gerrit_url + query, auth=self.auth)
             if response.status_code == 200:
                 json_data = self.clean_json(response.text)
